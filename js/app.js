b80764e0efb41e3415c958ef644b88e29fbc941b
netstermonir <monirhossain6163@gmail.com>
2021-08-29 17:19:47 +0600

    bug fix

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ document.getElementById('search-btn').addEventListener('click', function (event)
 const displayerror = error => {
     const message = document.getElementById('error-message');
     message.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+  <strong>Your Location!</strong> Search Not Found.
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
